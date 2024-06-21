ab5081111bf399505ef725e485b493a2d891e567
web coders <webcoders1122@gmail.com>
2024-06-21 09:06:48 +0500

    server is now properly working with download and error

--- Server/index.js
+++ Server/index.js
@@ -7,11 +7,10 @@ const app = express();
 const port = 5050;
 
 // middlewhare
-// app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(instagramScrapper.getInstagramVideo);
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(instagramScrapper.getInstagramVideo);
 app.use("/instagram", instagramRouter.router);
 
 app.listen(port, () => console.log(`server started on port ${port}!`));
