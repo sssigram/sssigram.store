3c4de68d54a46f171e6a8fc0a0e3c9b2fdf20ce6
web coders <webcoders1122@gmail.com>
2024-06-22 22:26:55 +0500

    home downloader is working now

--- src/App.jsx
+++ src/App.jsx
@@ -11,46 +11,124 @@ import DownloadApp from "./components/DownloadApp";
 import { allFeatureCardData } from "./data/featureCard";
 import { howToDownloadData } from "./data/howToDownload";
 import { faqData } from "./data/faqData";
+import { useInstagram } from "./context/InstagramContext";
+import { FaDownload } from "react-icons/fa6";
+import { LuRefreshCcw } from "react-icons/lu";
+import { PropagateLoader } from "react-spinners";
+import { Link } from "react-router-dom";
 
 const App = () => {
+  const { instagramState, handleReset } = useInstagram();
+
   return (
     <div className='flex flex-col items-center justify-center'>
       <Header
         heading='Instagram Downloader'
         desc='Download Videos, Reels, Photos, IGTV, carousel from Instagram'
       />
-      <BodyContainer>
-        <div className='flex flex-col gap-1.5 items-center my-6'>
-          <h3 className='text-4xl font-bold mb-4 text-violet-700'>
-            Download Instagram IGTV Videos
-          </h3>
-          <p className='text-lg'>
-            Downloading Instagram videos with the IGTV video downloader is easy,
-            fast, and time-saving. When browsing through Instagram or coming
-            across a post with an IGTV video you want to download, simply copy
-            the URL of that IGTV video and paste it into the designated field on
-            the iGram page. Finally, click the Download button. That's it!
-          </p>
-        </div>
-        {/* How to download section start */}
-        <div>
-          <Heading>HOW TO DOWNLOAD ANYTHING FROM INSTAGRAM?</Heading>
-          <p className='text-lg text-center'>
-            Below are the three easy and quickest steps to download an Instagram
-            Anything:
-          </p>
-        </div>
-        <HowToDownload data={howToDownloadData} />
-        {/* How to download section end */}
+      {instagramState.loading ? (
+        <BodyContainer>
+          <PropagateLoader
+            className='m-10'
+            color='#7554a7'
+          />
+        </BodyContainer>
+      ) : instagramState.downloadObj.video != "" &&
+        instagramState.error === "" ? (
+        // download card
+        <BodyContainer>
+          <div className='bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w-full flex flex-col items-center md:items-stretch md:flex-row justify-center gap-10 m-5 p-5 md:px-0'>
+            <img
+              className='rounded-lg w-60'
+              src={instagramState.downloadObj.thumb}
+              alt=''
+            />
+            <div className='flex flex-col items-center justify-between'>
+              <div>
+                <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+                  Your Requested File is ready to Download...!!!
+                </h5>
+                <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
+                  Click the Download button below for saving file to your
+                  device.
+                </p>
+              </div>
+              <div className='flex flex-col gap-5 w-full'>
+                <Link
+                  to={instagramState.downloadObj.video}
+                  className='inline-flex items-center w-full justify-center py-4 px-3 text-sm  text-white bg-gradient-to-br from-blue-700 to-blue-500 rounded-md  hover:bg-gradient-to-br hover:from-[#c73a3a] hover:from-10% hover:via-[#7554a7] hover:via-60% hover:to-[#3f73e4] hover:to-100% focus:ring-4 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+                  <FaDownload
+                    size={24}
+                    className='pr-1.5'
+                  />
+                  <span className='text-md md:text-xl'>Download</span>
+                </Link>
+                <button
+                  onMouseDown={handleReset}
+                  className='inline-flex items-center w-full justify-center py-4 px-3  text-sm text-white bg-green-500 hover:bg-green-600 focus:ring-green-300 focus:ring-4 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 rounded-md'>
+                  <LuRefreshCcw
+                    size={28}
+                    className='pr-2'
+                  />
+                  <span className='text-md md:text-xl'>Refresh Page</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </BodyContainer>
+      ) : instagramState.error != "" &&
+        instagramState.downloadObj.video === "" ? (
+        <BodyContainer>
+          <div className='m-8'>
+            <p className='font-semibold text-xl sm:text-2xl md:text-3xl text-red-500 text-center mb-5'>
+              {instagramState.error}
+            </p>
+            <button
+              onMouseDown={handleReset}
+              className='inline-flex items-center w-full justify-center py-4 px-3  text-sm text-white bg-green-500 hover:bg-green-600 focus:ring-green-300 focus:ring-4 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 rounded-md'>
+              <LuRefreshCcw
+                size={28}
+                className='pr-2'
+              />
+              <span className='text-md md:text-xl'>Refresh Page</span>
+            </button>
+          </div>
+        </BodyContainer>
+      ) : (
+        <BodyContainer>
+          <div className='flex flex-col gap-1.5 items-center my-6'>
+            <h3 className='text-4xl font-bold mb-4 text-violet-700'>
+              Download Instagram IGTV Videos
+            </h3>
+            <p className='text-lg'>
+              Downloading Instagram videos with the IGTV video downloader is
+              easy, fast, and time-saving. When browsing through Instagram or
+              coming across a post with an IGTV video you want to download,
+              simply copy the URL of that IGTV video and paste it into the
+              designated field on the iGram page. Finally, click the Download
+              button. That's it!
+            </p>
+          </div>
+          {/* How to download section start */}
+          <div>
+            <Heading>HOW TO DOWNLOAD ANYTHING FROM INSTAGRAM?</Heading>
+            <p className='text-lg text-center'>
+              Below are the three easy and quickest steps to download an
+              Instagram Anything:
+            </p>
+          </div>
+          <HowToDownload data={howToDownloadData} />
+          {/* How to download section end */}
 
-        {/* featureCard section start */}
-        <Heading>Your Instagram Video Downloader Features</Heading>
-        <FeatureCard data={allFeatureCardData} />
-        {/* featureCard section end */}
-        <FeatureSection />
-        <FAQs data={faqData} />
-        <DownloadApp />
-      </BodyContainer>
+          {/* featureCard section start */}
+          <Heading>Your Instagram Video Downloader Features</Heading>
+          <FeatureCard data={allFeatureCardData} />
+          {/* featureCard section end */}
+          <FeatureSection />
+          <FAQs data={faqData} />
+          <DownloadApp />
+        </BodyContainer>
+      )}
     </div>
   );
 };
