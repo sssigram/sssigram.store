bef837418a80a1f3ef0a9a973026a20da1da0ce8
web coders <webcoders1122@gmail.com>
2024-06-22 16:00:40 +0500

    faq page added

--- src/pages/FAQsPage.jsx
+++ src/pages/FAQsPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import FAQs from "../components/FAQs";
+import { faqData } from "../data/faqData";
+import BodyContainer from "../components/BodyContainer";
 
 const FAQsPage = () => {
-  return <div>FAQsPage</div>;
+  return (
+    <div className='flex justify-center items-center'>
+      <BodyContainer>
+        <FAQs data={faqData} />
+      </BodyContainer>
+    </div>
+  );
 };
 
 export default FAQsPage;
